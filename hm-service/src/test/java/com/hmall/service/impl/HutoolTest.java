208b7ded5c3d8833c67aae006c6089a9d14518aa
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-06-26 15:48:08 +0800

    first commit

---
+++ hm-service/src/test/java/com/hmall/service/impl/HutoolTest.java
@@ -0,0 +1,13 @@
+package com.hmall.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+public class HutoolTest {
+
+
+
+}
