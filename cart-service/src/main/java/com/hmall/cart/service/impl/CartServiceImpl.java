707f27bbe9735604165f29380e1c8d93190a6eb0
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-20 11:53:41 +0800

    购物车模块完成 缺失商品rpc

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -3,13 +3,17 @@ package com.hmall.cart.service.impl;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.domain.dto.CartFormDTO;
+import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
+import com.hmall.cart.domain.vo.CartVO;
 import com.hmall.cart.mapper.CartMapper;
 import com.hmall.cart.service.ICartService;
 import com.hmall.common.exception.BizIllegalException;
 import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
@@ -18,29 +22,28 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Service
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
-    private final IItemService itemService;
+//    private final IItemService itemService;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
-        // 1.获取登录用户
-        Long userId = UserContext.getUser();
-
-        // 2.判断是否已经存在
+        //1.查询用户的id，来获取购物车信息
+        long userId = UserContext.getUser();
+        //2.判断是否存在
         if(checkItemExists(cartFormDTO.getItemId(), userId)){
-            // 2.1.存在，则更新数量
+            //2.1存在，则更新数量
             baseMapper.updateNum(cartFormDTO.getItemId(), userId);
             return;
         }
-        // 2.2.不存在，判断是否超过购物车数量
+        //2.2 不存在，判断1是否超过购物车数量
         checkCartsFull(userId);
-
-        // 3.新增购物车条目
-        // 3.1.转换PO
+        //3.新增购物车条目
+        //3.1转换po
         Cart cart = BeanUtils.copyBean(cartFormDTO, Cart.class);
-        // 3.2.保存当前用户
+        //3.2 保存当前用户
         cart.setUserId(userId);
-        // 3.3.保存到数据库
+        //3.3保存到数据库
         save(cart);
     }
 
@@ -63,25 +66,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     }
 
     private void handleCartItems(List<CartVO> vos) {
-        // 1.获取商品id
-        Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
-        // 2.查询商品
-        List<ItemDTO> items = itemService.queryItemByIds(itemIds);
-        if (CollUtils.isEmpty(items)) {
-            return;
-        }
-        // 3.转为 id 到 item的map
-        Map<Long, ItemDTO> itemMap = items.stream().collect(Collectors.toMap(ItemDTO::getId, Function.identity()));
-        // 4.写入vo
-        for (CartVO v : vos) {
-            ItemDTO item = itemMap.get(v.getItemId());
-            if (item == null) {
-                continue;
-            }
-            v.setNewPrice(item.getPrice());
-            v.setStatus(item.getStatus());
-            v.setStock(item.getStock());
-        }
+
     }
 
     @Override
